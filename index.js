fe4625846721c5dd1dcb8abb1e5faa97a11081b4
Alexander <alykoshin@gmail.com>
2016-03-10 11:53:49 +0300

    renamed orig end handler

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var debug = require('debug')('end');
 
 var endMw = function(req, res, next) {
 
-  if (!res._queue_orig_end_handler) {
+  if (!res._orig_end_handler) {
 
-    res._queue_orig_end_handler = res.end;
+    res._orig_end_handler = res.end;
 
     res.end = function () {
-      res.end = res._queue_orig_end_handler;
+      res.end = res._orig_end_handler;
       res.emit('end');
       res.end.apply(this, arguments);
     };
