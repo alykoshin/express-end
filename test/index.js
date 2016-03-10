3239c121e311462d78acd289812f6b10e300cc70
Alexander <alykoshin@gmail.com>
2016-03-10 11:45:01 +0300

    added one more tests, reached 100% coverage

--- test/index.js
+++ test/index.js
@@ -46,6 +46,28 @@ describe('express-end', function () {
 
   });
 
+  it('should avoid to overrride res.end() twice', function (done) {
+    var resEnd = function() {};
+    res.end = resEnd;
+    expect(res._queue_orig_end_handler).is.undefined;
+
+    endMw(req, res, function() {
+      expect(res._queue_orig_end_handler).equals(resEnd);
+      expect(res.end).is.not.undefined;
+      var resEnd2 = res.end;
+
+      endMw(req, res, function() {
+        // Originally stored res.end() was not changed
+        expect(res._queue_orig_end_handler).equals(resEnd);
+        // res.end() points to same MW's handler
+        expect(res.end).equals(resEnd2);
+        done();
+      });
+
+    });
+
+  });
+
   it('should emit `end` on `res.end()`', function (done) {
     var endArgs = [ 'arg1', 'arg2', 'arg3' ];
 
